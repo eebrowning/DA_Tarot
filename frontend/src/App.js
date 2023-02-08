1c7ed6ca4d2dbe2c679c28135b52b4cd37161b29
eebrowning <eebrowning828@gmail.com>
2023-02-08 14:56:33 -0700

    animation fun

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -33,8 +33,10 @@ function App() {
           </Switch>
         </div>
       </BrowserRouter>
-
-      <img className="inq-logo" src='https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/59bc2eb0-69b1-42c5-a929-a029c3a783fc/d8atpo0-589deb6a-04fe-49d8-be76-d08411c464cf.png/v1/fill/w_1280,h_720,q_80,strp/dragon_age_inquisition_wallpaper_by_seigner_d8atpo0-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzIwIiwicGF0aCI6IlwvZlwvNTliYzJlYjAtNjliMS00MmM1LWE5MjktYTAyOWMzYTc4M2ZjXC9kOGF0cG8wLTU4OWRlYjZhLTA0ZmUtNDlkOC1iZTc2LWQwODQxMWM0NjRjZi5wbmciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.tfpsWn3m-5bXPCtJxQt0nJ3jswa-rMc_9YL8YCs-d1M'></img>
+      <div className="logo-pulse">
+        <img className="inq-logo" src='https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/59bc2eb0-69b1-42c5-a929-a029c3a783fc/d8atpo0-589deb6a-04fe-49d8-be76-d08411c464cf.png/v1/fill/w_1280,h_720,q_80,strp/dragon_age_inquisition_wallpaper_by_seigner_d8atpo0-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzIwIiwicGF0aCI6IlwvZlwvNTliYzJlYjAtNjliMS00MmM1LWE5MjktYTAyOWMzYTc4M2ZjXC9kOGF0cG8wLTU4OWRlYjZhLTA0ZmUtNDlkOC1iZTc2LWQwODQxMWM0NjRjZi5wbmciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.tfpsWn3m-5bXPCtJxQt0nJ3jswa-rMc_9YL8YCs-d1M'></img>
+        <div className='pulse-circle' ></div>
+      </div>
       <div className="clouds"></div>
       <div className='app-background'>
 
