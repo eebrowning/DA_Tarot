6af4b4bb776a9abb92337415deec6ab764b7e320
eebrowning <eebrowning828@gmail.com>
2023-02-12 17:38:04 -0700

    switching up forms

--- frontend/src/components/AthleteForm/Part-2.js
+++ frontend/src/components/AthleteForm/Part-2.js
@@ -1,5 +1,11 @@
 import { useEffect, useState } from "react";
+import './part-2.css'
 
+
+
+////TODO for dragon age app: make radio options for gender, after gender selected, move to class.
+//class radio options appear, after selection, next enabled.
+//this will be the test of the DA:Inquisition-like character detail menu.
 const Part2 = ({ receivePart2, step }) => {
     // let sportsArr = [
     //     'Golf', 'Tennis', 'Cricket', 'Basketball', 'Baseball', 'American Football', 'Aquatics', 'Archery', 'Automobile Racing', 'Badminton', 'Beach Volleyball', 'Bobsleigh', 'Body Building', 'Boxing', 'Cross Country Running', 'Cross Country Skiing', 'Curling', 'Cycling', 'Darts', 'Decathlon', 'Down Hill Skiing', 'Equestrianism', 'eSports', 'Fencing', 'Field Hockey', 'Figure Skating', 'Gymnastics', 'Ice Hockey', 'Martial Arts', 'Mixed Martial Arts', 'Modern Pentathlon', 'Motorcycle Racing', 'Netball', 'Polo', 'Racquetball', 'Rowing', 'Rugby', 'Sailing', 'Softball', 'Shooting', 'Skateboarding', 'Skeet Shooting', 'Skeleton', 'Snow Boarding', 'Soccer (Football)', 'Squash', 'Surfing', 'Swimming', 'Track and Field'
@@ -7,24 +13,46 @@ const Part2 = ({ receivePart2, step }) => {
     let sportsArr = ["Mage", "Warrior", "Rogue"];
     const [gender, setGender] = useState();
     const [sports, setSports] = useState();
-    const [birthdate, setBirthdate] = useState();
+    // const [birthdate, setBirthdate] = useState();
+    const [birthdate, setBirthdate] = useState("1111-11-11T00:00:00.000Z");//temp to disable input TODO
+
 
     useEffect(() => {
         receivePart2({ gender, sports, birthdate })
     }, [gender, sports, birthdate])
 
     return (<div style={{ display: step === 2 ? 'flex' : "none" }} className="part-2 bootPart">
-        <label htmlFor="sports" className="form-label">Sport
+        <label htmlFor="sports" className="form-label">Select Class
             <label id='sports-error' htmlFor="sports"></label>
-            <select name='sports' id='sports-select' className="form-control boot-data" onChange={e => setSports(e.target.value)} required>
+            {/* 
+            KEEP, former form structure
+            <select  name='sports' id='sports-select' className="form-control boot-data" onChange={e => setSports(e.target.value)} required>
                 <option>{null}</option>
                 {
                     sportsArr.map(sport => (
                         <option key={sport}>{sport}</option>
                     ))
                 }
-            </select>
-        </label>
+            </select> 
+            */}
+            <div name='sports' id='sports-select' >
+                {
+                    sportsArr?.map(sport => (
+                        <div class="form-check form-check-inline sport" >
+                            <input type='radio' className="form-check-input" name="inlineRadioOptions" value={sport} id={`inlineRadio${sport}`} />
+                            {sport}
+                            {/* <label className="form-check-label" htmlFor={`inlineRadio${sport}`}>
+                            <img href="https://i.pinimg.com/736x/cf/86/65/cf8665820bced2d38bc7cd125e66f104--dragon-age-inquisition-tarot-cards.jpg" alt={""} />
+                            </label> */}
+                        </div>
+                    ))
+                }
+            </div>
+        </label >
+
+
+
+
         <label htmlFor="gender-select" className="form-label">Gender
             <label id='gender-error' htmlFor="gender-select"></label>
             <select onChange={e => setGender(e.target.value)} id='gender' className="form-control boot-data" required>
@@ -35,7 +63,8 @@ const Part2 = ({ receivePart2, step }) => {
 
             </select>
         </label>
-        <label htmlFor="birthdate" className="form-label">Birthdate
+        {/* temp disable  */}
+        {/* <label htmlFor="birthdate" className="form-label">Birthdate
             <label id='birthdate-error' htmlFor="birthdate"></label>
             <input
                 required
@@ -46,8 +75,8 @@ const Part2 = ({ receivePart2, step }) => {
 
                 onChange={e => setBirthdate(e.target.value)}
             />
-        </label>
-    </div>)
+        </label> */}
+    </div >)
 }
 
 export default Part2;
