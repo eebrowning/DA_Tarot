d48e1eee1066895e14af78c7da25c0478cb19d66
eebrowning <eebrowning828@gmail.com>
2023-02-15 18:05:21 -0700

    almost happy with select carousel

--- frontend/src/components/AthleteForm/Part-2.js
+++ frontend/src/components/AthleteForm/Part-2.js
@@ -1,90 +1,122 @@
 import { useEffect, useState } from "react";
 import './part-2.css'
 import classData from "../../util/classData";
-
+import raceData from '../../util/raceData'
+import moveToSelected from "../../util/moveToSelected";
 
 ////TODO for dragon age app: make radio options for gender, after gender selectedSport, move to class.
 //class radio options appear, after selection, next enabled.
 //this will be the test of the DA:Inquisition-like character detail menu.
 const Part2 = ({ receivePart2, step }) => {
-    let testData = Object.values(classData);
-    let testData2 = Object.keys(classData);
-
+    let classValues = Object.values(classData);
     let sportsArr = [];
-    testData.forEach(entry => { sportsArr.push(entry['class']) })
+    classValues.forEach(entry => { sportsArr.push(entry['class']) })
     const [gender, setGender] = useState();
     const [sports, setSports] = useState();
     // const [birthdate, setBirthdate] = useState();
     const [birthdate, setBirthdate] = useState("1111-11-11T00:00:00.000Z");//temp to disable input TODO
-    const [select, setSelect] = useState();
+    // const carouselLabels = ["prevLeftSecond", "prev", "selected", "next", "nextRightSecond"];
+    //some wiggle room with my carousel: allows me to pick starting point
+    const carouselLabels = ["selected", 'next', 'nextRightSecond', 'hideRight', 'hideRight'];
+    // const carouselLabels = ["selected", 'next', 'nextRightSecond'];
+
+
+    const [carouselCards, setCarouselCards] = useState([])
+
 
     useEffect(() => {
         receivePart2({ gender, sports, birthdate })
         console.log(sports, 'did class change?')
     }, [gender, sports, birthdate])
     useEffect(() => {
-        document.getElementById('sports-select')?.firstChild.classList.add('selectedSport')
+        setCarouselCards(document.getElementById("sports-select").children)
+        handleLabels();
+        setSports(document.querySelector('.sport')?.firstChild.value)
+
+        console.log(document.querySelector('.selected'));
 
     }, [])
-    let handleSelect = (e) => {
-        e.preventDefault();
-        setSports(document.querySelector('.selectedSport').firstChild.value)
 
+
+    let handleLabels = () => {
+        for (let i = 0; i < carouselLabels.length; i++) {
+            document.getElementsByClassName('sport')[i]?.classList.remove('hideRight')
+            document.getElementsByClassName('sport')[i]?.classList.add(carouselLabels[i])
+        }
     }
     let handleClickLeft = (e) => {
         e.preventDefault();
-        document.querySelector('.selectedSport')?.previousSibling.classList.add('selectedSport')
-        document.getElementsByClassName('selectedSport')[1].classList.remove('selectedSport')
-        setSports(document.querySelector('.selectedSport').firstChild.value)
+        // setSports(document.querySelector('.selected').firstChild.value)
+
+        // document.querySelector('#sports-select .selected')?.previousSibling.classList.add('selected')
+        // document.querySelectorAll('#sports-select .selected')[1]?.classList.remove('selected')
+        moveToSelected('prev')
+
+        setSports(document.querySelector('#sports-select  .selected')?.firstChild.value)
+
+
 
     }
     let handleClickRight = (e) => {
         e.preventDefault();
-        document.querySelector('.selectedSport')?.nextSibling.classList.add('selectedSport')
-        document.querySelector('.selectedSport').classList.remove('selectedSport')
-        setSports(document.querySelector('.selectedSport').firstChild.value)
 
+        moveToSelected('next');
+
+        console.log(document.querySelector('#sports-select  .selected'), 'selected class in .sports');
+        setSports(document.querySelector('#sports-select  .selected')?.firstChild.value)
+
+    }
+
+    let handleSelect = (e) => {
+        e.preventDefault();
+        // redundant
+        setSports(document.querySelector('#sports-select .selected').firstChild.value)
     }
     let handleClickPicture = (e) => {
         e.preventDefault();
-        document.querySelector('.selectedSport')?.classList.remove('selectedSport');
-        document.getElementById(e.target.id + '-card').classList.add('selectedSport');
-        setSports(document.querySelector('.selectedSport').firstChild.value)
+        document.querySelector('.selected')?.classList.remove('selected');
+        document.getElementById(e.target.id + '-card').classList.add('selected');
+        setSports(document.querySelector('#sports-select .selected').firstChild.value)
     }
 
     return (<div style={{ display: step === 2 ? 'flex' : "none" }} className="part-2 bootPart">
+
+        <label htmlFor="gender-select" className="form-label">Gender
+            <label id='gender-error' htmlFor="gender-select"></label>
+            <select onChange={e => setGender(e.target.value)} id='gender' className="form-control boot-data" required>
+                <option>{null}</option>
+                <option>Male</option>
+                <option>Female</option>
+                <option>Other</option>
+
+            </select>
+        </label>
+
         <label htmlFor="sports" className="form-label">Select Class
             <label id='sports-error' htmlFor="sports"></label>
-
-
-            <label htmlFor="gender-select" className="form-label">Gender
-                <label id='gender-error' htmlFor="gender-select"></label>
-                <select onChange={e => setGender(e.target.value)} id='gender' className="form-control boot-data" required>
-                    <option>{null}</option>
-                    <option>Male</option>
-                    <option>Female</option>
-                    <option>Other</option>
-
-                </select>
-            </label>
-
-
             <div name='sports' id='sports-select'>
-                {
-                    sportsArr?.map(sport => (
-                        <div className={`form-check form-check-inline sport`} id={`${sport}-card`} key={`${sport}`}>
-                            <input type='radio' className="form-check-input " name="inlineRadioOptions" value={sport} id={`inlineRadio${sport}`} />
-                            <label onClick={handleClickPicture} id={sport} className="form-check-label" htmlFor={`inlineRadio${sport}`} style={{ backgroundImage: `URL(${classData[sport]["cardURL"]})` }}>
-                                {sport}
-                            </label>
-                        </div>
-                    ))
-                }
-                {!document.getElementsByClassName('selectedSport') && document.getElementById('sports-select')?.firstChild.classList.add('selectedSport')}
+                <div>
+
+                    {
+                        sportsArr?.map(sport => (
+                            <div className={`form-check form-check-inline sport`} id={`${sport}-card`} key={`${sport}`}>
+                                <input type='radio' className="form-check-input " name="inlineRadioOptions" value={sport} id={`inlineRadio${sport}`} />
+                                <label onClick={handleClickPicture} id={sport} className="form-check-label" htmlFor={`inlineRadio${sport}`} style={{ backgroundImage: `URL(${classData[sport]["cardURL"]})` }}>
+                                    {sport}
+                                </label>
+                            </div>
+                        ))
+                    }
+                    {!document.getElementsByClassName('selectedSport') && document.getElementById('sports-select')?.firstChild.classList.add('selectedSport')}
+                </div>
             </div>
             <img className={'white-arrow'} onClick={handleClickLeft} id="prev" src='https://i.imgur.com/oTediJN.png' />
-            <button onClick={handleSelect}>Select {sports ? classData[sports]['general_class'] : null}</button>
+
+            {/* <button onClick={handleSelect} id='class-button'>Select </button> */}
+            <button onClick={handleSelect} id='class-button'>Select {sports ? classData[sports]['general_class'] : null}</button>
+
             <img className={'white-arrow'} onClick={handleClickRight} id="next" src='https://i.imgur.com/UpFYkCd.png' />
+
         </label >
 
         {/* 
---
+++ frontend/src/util/raceData.js
@@ -0,0 +1,33 @@
+
+
+let Qunari = {
+    race: "Qunari",
+    male: "url",
+    female: "url",
+    descriptor: 'Qunari'
+}
+
+let Human = {
+    race: "Human",
+    male: "url",
+    female: "url",
+    descriptor: 'Human'
+
+}
+
+let Dwarf = {
+    race: "Dwarf",
+    male: "url",
+    female: "url",
+    descriptor: 'Dwarven'
+}
+
+let Elf = {
+    race: "Elf",
+    male: "url",
+    female: "url",
+    descriptor: 'Elvhen'
+}
+
+
+module.exports = { Qunari, Human, Elf, Dwarf }
