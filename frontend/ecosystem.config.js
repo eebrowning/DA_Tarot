4ba296d66c13b43214f5329fe8f081038122b372
EC2 Default User <ec2-user@ip-172-31-17-116.us-west-1.compute.internal>
2023-03-29 19:27:08 +0000

    config update

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -2,6 +2,11 @@ module.exports = {
     apps: [{
         name: "frontend",
         script: "index.html",
-        cwd: "/home/ec2-user/DA_Tarot/frontend/build"
+        cwd: "/home/ec2-user/DA_Tarot/frontend/build",
+  	env: {
+    	    "NODE_ENV": "production",
+    	    "PORT": "3000",
+    	    "HOST": "0.0.0.0"
+  	}
     }]
 }
